696a5d174e776fee7bc765387adf73a8e0c27730
tehuanmelo <tehuanmelo@gmail.com>
2026-02-17 21:09:02 +0400

    fixing time with webkit

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,18 +4,24 @@ import { cn } from "@/lib/utils";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
+    const timeFix =
+      type === "time"
+        ? "block appearance-none [&::-webkit-date-and-time-value]:text-left [&::-webkit-date-and-time-value]:p-0 [&::-webkit-datetime-edit]:p-0"
+        : "";
+
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-green-500 px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-[16px] md:text-sm",
-          className,
+          "h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          timeFix,
+          className
         )}
         ref={ref}
         {...props}
       />
     );
-  },
+  }
 );
 Input.displayName = "Input";
 
